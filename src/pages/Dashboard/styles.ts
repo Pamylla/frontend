1b19f62edcfcb7ca8792e8beda4b2fd03bf8a4e3
Pamylla <pamyllaostermann@gmail.com>
2020-10-28 00:21:01 -0300

    Created Dashboard and Validations routes

---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Header = styled.header`
+  padding: 32px 0;
+  background: #28262e;
+`;
+
+export const HeaderContent = styled.div``;
